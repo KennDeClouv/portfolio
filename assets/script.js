99d88d5c150ddd1f2d5f233e4a4844be6bc38af0
izzulaverroes <146813701+izzulaverroes@users.noreply.github.com>
2024-03-27 08:12:12 +0700

    V1.1

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// BURGER TOGGLE
 const a = document.querySelector(".burger");
 const b = document.querySelector(".burger-body");
 
@@ -14,14 +15,33 @@ document.addEventListener("click", function (e) {
 
 const container = document.querySelector('.second-section')
 const sections = gsap.utils.toArray('.second-content')
+const content = document.querySelector('.second-content')
+gsap.registerPlugin(ScrollTrigger); 
 
-let scrollTween = gsap.to(sections,{
-  xPercent : -100 *(sections.length - 1),
-  ease:"none",
-  scrollTrigger:{
-    trigger: ".second-section",
-    pin: true,
-    scrub: 1,
-    end:'+=3000'
-  }
+
+let contentWidth = 0;
+// Iterate over each section to calculate the total width
+sections.forEach(section => {
+  contentWidth += section.offsetWidth;
+});
+
+// Calculate the amount to scroll horizontally
+let amountToScroll = contentWidth - window.innerWidth;
+
+
+
+const tween = gsap.to(content,{
+  x : -amountToScroll - 400,
+  duration : 3,
+  ease : "none"
+})
+
+ScrollTrigger.create({
+  trigger : ".second-section",
+  start : "top",
+  end : "+=" + amountToScroll,
+  pin : true,
+  animation : tween,
+  scrub : 1,
+  // markers : true
 })
