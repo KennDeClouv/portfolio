0d7ea5cf790d6afb2e86472a09072be52312c27b
Izzul <izzulaverroes@gmail.com>
2024-03-28 12:42:22 +0700

    V1.2

--- assets/script.js
+++ assets/script.js
@@ -12,40 +12,69 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// LOADER
+// document.addEventListener("DOMContentLoaded", () => {
+//   function calculatePercentage() {
+//     const totalElements = document.getElementsByTagName("*").length; // Total number of elements
+//     const loadedElements = document.querySelectorAll(
+//       "*"
+//     ).length; // Number of loaded elements
 
-const container = document.querySelector('.second-section')
-const sections = gsap.utils.toArray('.second-content')
-const content = document.querySelector('.second-content')
-gsap.registerPlugin(ScrollTrigger); 
+//     return Math.floor((loadedElements / totalElements) * 100); // Calculate percentage
+//   }
 
+//   // Update the <h1> element with the loading percentage
+//   function updatePercentage() {
+//     const percentage = calculatePercentage();
+//     document.querySelector("#DOM").textContent = `${percentage}%`;
+//   }
 
-let contentWidth = 0 ;
+//   // Call the updatePercentage function initially and attach it to the window's load event
+//   updatePercentage();
+//   window.addEventListener("load", updatePercentage);
+//   setTimeout(() => {
+//     document.querySelector(".transition").classList.add("loaded");
+//     setTimeout(()=>{
+//       document.body.removeChild(document.querySelector(".loader"));
+
+//     },2000)
+//     setTimeout(() => {
+//       document.body.removeChild(document.querySelector(".transition"));
+//     }, 6000);
+//   }, 1000);
+// });
+
+const container = document.querySelector(".second-section");
+const sections = gsap.utils.toArray(".second-content");
+const content = document.querySelector(".second-content");
+gsap.registerPlugin(ScrollTrigger);
+
+let contentWidth = 0;
 // Iterate over each section to calculate the total width
-sections.forEach(section => {
+sections.forEach((section) => {
   contentWidth += section.offsetWidth;
 });
 
 // Calculate the amount to scroll horizontally
 let amountToScroll = contentWidth - window.innerWidth;
 
-
 if (window.innerWidth > 768) {
-  const tween = gsap.to(content,{
-    x : -amountToScroll - 400,
-    duration : 3,
-    ease : "none"
-  })
-  
+  const tween = gsap.to(content, {
+    x: -amountToScroll - 400,
+    duration: 3,
+    ease: "none",
+  });
+
   ScrollTrigger.create({
-    trigger : ".second-section",
-    start : "top",
-    end : "+=" + amountToScroll,
-    pin : true,
-    animation : tween,
-    scrub : 1,
+    trigger: ".second-section",
+    start: "top",
+    end: "+=" + amountToScroll,
+    pin: true,
+    animation: tween,
+    scrub: 1,
     // markers : true
-  })  
+  });
 }
 
-const secondSectionsL = gsap.utils.toArray('.third-section_left')
-const secondSectionsR = gsap.utils.toArray('.third-section_right')
+const secondSectionsL = gsap.utils.toArray(".third-section_left");
+const secondSectionsR = gsap.utils.toArray(".third-section_right");
