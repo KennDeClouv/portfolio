aa271775456d24cc6d7e17bafc767cc2a6c25ad2
izzulaverroes <146813701+izzulaverroes@users.noreply.github.com>
2024-05-20 06:59:18 +0700

    responsive in dev

---
+++ assets/components/navbar.js
@@ -0,0 +1,84 @@
+const nav = document.querySelector("#navbox");
+nav.innerHTML = `
+<div class="burger-body">
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<ul>
+  <li>
+    <p id="toHome">Home</p>
+    <div></div>
+  </li>
+  <li>
+    <p id="toEdu">Education</p>
+    <div></div>
+  </li>
+  <li>
+    <p id="toCont">Contact</p>
+    <div></div>
+  </li>
+</ul>
+</div>
+
+<nav id="nav">
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<div class="burger">
+  <!-- <h1>HIRE</h1> -->
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+<div class="frame"></div>
+<div class="frame-r"></div>
+<div class="frame-b"></div>
+<div class="frame-l"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none" class="frame-svg">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none"
+  class="frame-svg-r">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none"
+  class="frame-svg-b">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+
+<div class="logo logo-active">
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+    <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#999" />
+  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+    <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+    <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+  </svg>
+  <a href="#" class="logo-zull m-auto">
+    <span>zu</span><br /><span>ll</span>
+  </a>
+</div>
+</nav>
+
+<div class="sound-bar-wrapper active off">
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+  <path d="M0 0H6.5H13V6.5V13C13 3.25 9.75 0 0 0Z" fill="#fff" />
+</svg>
+<p id="soundOnText">On</p>
+<p id="soundOffText">Off</p>
+</div>`
---
+++ assets/components/second-section.js
@@ -0,0 +1,77 @@
+const secondSec = document.querySelector("#second-section")
+secondSec.innerHTML = `
+<h1 class="main-content d-none d-md-inline text-nowrap" id="ss_main-content" style="z-index: 100;">
+<span style="font-weight: 900;">&emsp;Hello ..</span>
+<span style="font-weight: 100;"> Im</span><span
+  style="-webkit-text-stroke: 3px #212121;color: transparent; font-weight: 500;"> Muhammad Ken
+  Izzulhaq</span><span style="font-weight: 900; text-transform: uppercase;"> Programmer</span>&nbsp;<span
+  style="font-weight: 100; -webkit-text-stroke: #fff 2px;color: transparent; background-color: #212121; padding: 0 10px;border-radius: 10px;">
+  and </span> &nbsp;<span style="font-weight: 900; text-transform: uppercase;">Designer</span>
+who combines <span style="text-transform: uppercase; font-weight: 700;">beauty</span><span
+  style="font-weight: 100; -webkit-text-stroke: #212121 2px;color: transparent;font-family: fk;"> and
+</span><span style="text-transform: uppercase; font-weight: 700;">functionality</span> in every <span
+  style="text-transform: uppercase; color: transparent; -webkit-text-stroke: #212121 2px;">design.</span>
+</h1>
+<h1 class="d-block d-md-none fs-6 text-center" style="z-index: 100;">Hello .. Im Muhammad Ken Izzulhaq <span style="font-weight:600">PROGRAMMER</span>  and  <span style="font-weight:600">DESIGNER</span> who combines beauty and functionality in every <span style="font-weight:600">DESIGN.</span>
+</h1>
+<div class="w-100 position-absolute z-3">
+<div class="row text-center plus-play">
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
+      <path
+        d="M0 2C0 0.89541 0.895313 0 2 0H7C8.10469 0 9 0.89541 9 2V6C9.00078 8.09824 9.80078 8.99756 12 8.99756L11.9023 8.99814L11.8035 9H16C17.1047 9 18 9.89541 18 11V15C18.0008 17.0982 18.8008 17.9976 21 17.9976C20.9332 17.9976 20.868 17.9983 20.8035 18H24.1945C24.1309 17.9983 24.066 17.9976 24 17.9976C26.0992 17.9976 26.9992 17.1981 27 15V11C27 9.89541 27.8953 9 29 9H33C35.1 9 36 8.2 36 6V2C36 0.89541 36.8953 0 38 0H43C44.1047 0 45 0.89541 45 2V7C45 8.10459 44.1047 9 43 9H39C36.9 9 36 9.8 36 12V16C36 17.1046 35.1047 18 34 18H29.8035C27.8438 18.0498 27 18.8675 27 20.9976V24C27.0008 26.0982 27.8008 26.9976 30 26.9976L29.9258 26.9979L29.8035 27H34C35.1047 27 36 27.8954 36 29V33C36.0008 35.0982 36.8008 35.9976 39 35.9976C38.9332 35.9976 38.868 35.9983 38.8035 36H43C44.1047 36 45 36.8954 45 38V43C45 44.1046 44.1047 45 43 45H38C36.8953 45 36 44.1046 36 43V38.9976C36 36.9606 35.2477 36.0526 33.1945 36H29C27.8953 36 27 35.1046 27 34V29.9976C27 27.9606 26.2477 27.0526 24.1945 27H20.8035C18.8438 27.0498 18 27.8675 18 29.9976V34C18 35.1046 17.1047 36 16 36H11.8035C9.84375 36.0498 9 36.8675 9 38.9976V43C9 44.1046 8.10469 45 7 45H2C0.895313 45 0 44.1046 0 43V38C0 36.8954 0.895313 36 2 36H6.19453C6.13086 35.9983 6.06602 35.9976 6 35.9976C8.09922 35.9976 8.99922 35.1981 9 33V29C9 27.8954 9.89531 27 11 27H15.1945C15.1309 26.9983 15.066 26.9976 15 26.9976C17.0992 26.9976 17.9992 26.1981 18 24V20.9976C18 18.9606 17.2477 18.0526 15.1945 18H11C9.89531 18 9 17.1046 9 16V11.9976C9 9.96064 8.24766 9.05264 6.19453 9H2C0.895313 9 0 8.10459 0 7V2Z"
+        fill="black" />
+    </svg>
+  </div>
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <div class="col-3">
+    <h3 class="d-none d-md-block">27</h3>
+  </div>
+</div>
+<div class="row text-center plus-play" style="margin-top: 30vh;">
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <div class="col-3">
+  </div>
+  <div class="col-3">
+  </div>
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+</div>
+<div class="row text-center plus-play" style="margin-top: 30vh;">
+  <div class="col-3 d-none d-md-block">
+    <h5 id="scramble">ALL ABOUT DESIGN.</h5>
+  </div>
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <div class="col-3">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <div class="col-3 d-none d-md-block">
+    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+      <path d="M14.5 1V27M1 14H28" stroke="#33333350" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+</div>
+
+</div>`
